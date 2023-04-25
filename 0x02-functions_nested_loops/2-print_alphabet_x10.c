4b6ed5a68a6732461f98f64d77b2f15b91970f5c
Benedicta1 <benedictaojogwu@gmail.com>
2023-04-25 06:36:07 -0700

    2-print_alphabet_x10.c

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - prints 10 times the alphabet
+ * in lowercase
+ * Return: Always 0
+ */
+void print_alphabet_x10(void)
+{
+char be;
+int x = 0;
+
+while (x <= 9)
+{
+	for (be = 'a'; be <= 'z'; be++)
+	{
+	_putchar(x);
+	}
+	_putchar('\n');
+
+	be++;
+}
+}
