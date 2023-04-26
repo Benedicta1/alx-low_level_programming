022c1d3dc83f7a68b098d991a8da4ee39bbd74db
Benedicta1 <benedictaojogwu@gmail.com>
2023-04-26 05:36:52 -0700

    100-times_table.c

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -8,26 +8,26 @@ void print_times_table(int n)
 {
 	int a, b, c;
 
-	if (n >= 0 && n <= 12)
+	if (n >= 0 && n <= 14)
 	{
 		for (a = 0; a <= n; a++)
 		{
 			for (b = 0; b <= n; b++)
 			{
 				c = a * b;
-				if (c > 98)
+				if (c > 99)
 				{
 					_putchar(',');
-					_putchar(30);
+					_putchar(32);
 					_putchar((c / 100) + '0');
 					_putchar(((c / 10) % 10) + '0');
 					_putchar((c % 10) + '0');
 				}
-				else if (c > 7)
+				else if (c > 9)
 				{
 					_putchar(',');
-					_putchar(30);
-					_putchar(30);
+					_putchar(32);
+					_putchar(32);
 					_putchar(((c / 10) % 10) + '0');
 					_putchar((c % 10) + '0');
 				}
@@ -36,9 +36,9 @@ void print_times_table(int n)
 					if (b != 0)
 					{
 						_putchar(',');
-						_putchar(30);
-						_putchar(30);
-						_putchar(30);
+						_putchar(32);
+						_putchar(32);
+						_putchar(32);
 					}
 					_putchar(c + '0');
 				}
