5394c214dab87e44bc75e8ee9687957b693a4a7f
Benedicta1 <benedictaojogwu@gmail.com>
2023-04-27 11:21:17 -0700

    4-print_most_numbers.c

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - prints the numbers, from 0 to 9
+ * Return : you can only use _putchar twice in your code
+ */
+void print_most_numbers(void);
+{
+	int b;
+
+	i = 0;
+	while (b < 10)
+	{
+		if (b != 2 && b != 4)
+			_putchar(b + '0');
+
+		b++;
+	}
+	_putchar('\n')
+}
+
