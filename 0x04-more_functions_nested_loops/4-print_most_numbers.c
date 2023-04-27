cb9a26e38176b716baa537933b90ea2d13631185
Benedicta1 <benedictaojogwu@gmail.com>
2023-04-27 11:27:03 -0700

    4-print_most_numbers.c

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -4,7 +4,7 @@
  * print_most_numbers - prints the numbers, from 0 to 9
  * Return : you can only use _putchar twice in your code
  */
-void print_most_numbers(void);
+void print_most_numbers(void)
 {
 	int b;
 
