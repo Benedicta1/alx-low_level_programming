6cc0931a462389f93fec7f51fd3a7f07c6b6456f
Benedicta1 <benedictaojogwu@gmail.com>
2023-04-27 11:51:51 -0700

    5-more_numbers.c

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -6,16 +6,12 @@
  */
 void print_most_numbers(void)
 {
-	int b;
-
-	b = 0;
-	while (b < 10)
+	char b;
+	
+	for (b = '0'; b <= '9'; b++)
 	{
-		if (b != 2 && b != 4)
-			_putchar(b + '0');
-
-		b++;
+		if (!(b == '2' || b == '4'))
+			_putchar(b);
 	}
-	_putchar('\n')
+	_putchar('\n');
 }
-
