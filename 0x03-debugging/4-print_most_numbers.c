751e2b62b317cef70d0d58f4d5b91236b2b9ad5b
Benedicta1 <benedictaojogwu@gmail.com>
2023-04-28 07:16:57 -0700

    4-print_most_numbers.c

--- 0x03-debugging/4-print_most_numbers.c
+++ 0x03-debugging/4-print_most_numbers.c
@@ -8,12 +8,15 @@
 
 void print_most_numbers(void)
 {
-	char c;
+	int c;
 
-	for (c = '0'; c <= 9; c++)
+	c = 0;
+	while (c < 10)
 	{
-		if (!(c == '2' || c == '4'))
-			_putchar(c);
+		if (c != 2 && c != 4)
+			_putchar (c + '0');
+
+		c++;
 	}
-	_putchar('\n');
+	_putchar ('\n')
 }
