6ca536822186a7f32091e2a89af65f114882ee52
Benedicta1 <benedictaojogwu@gmail.com>
2023-05-03 00:00:42 -0700

    100-atoi.c

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -6,27 +6,27 @@
  */
 int _atoi(char *s)
 {
-	int a, b, c, d, e f;
+	int a, b, c, len, e, num;
 
 	a = 0;
 	b = 0;
 	c = 0;
-	d = 0;
+	len = 0;
 	e = 0;
-	f = 0;
+	num = 0;
 
-	while (s[d] != '\0')
-		d++;
-	while (a < d && e == 0)
+	while (s[len] != '\0')
+		len++;
+	while (a < len && e == 0)
 	{
 		if (s[a] == '-')
 			++b;
 		if (s[a] >= '0' && s[a] <= '9')
 		{
-			f = s[a] - '0';
+			num = s[a] - '0';
 			if (b % 2)
-				f = -f;
-			c = c * 10 + f;
+				num = -num;
+			c = c * 10 + num;
 			e = 1;
 			if (s[a + 1] < '0' || s[a + 1] > '9')
 				break;
