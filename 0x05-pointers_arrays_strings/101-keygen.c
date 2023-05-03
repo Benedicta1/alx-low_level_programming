0e53f23f259c8aee41ff2c60a2acb3a2ef4d4c1a
Benedicta1 <benedictaojogwu@gmail.com>
2023-05-03 00:13:03 -0700

    101-keygen.c

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -19,7 +19,7 @@ int main(void)
 	{
 		pass[a] = rand() % 78;
 		sum += (pass[a] + '0');
-		putcahr(pass[a] + '0');
+		putchar(pass[a] + '0');
 		if ((2772 - sum) - '0' < 78)
 		{
 			b = 2772 - sum - '0';
