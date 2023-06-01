e3f57dd0c97c2c8f348128c0730e1a555c3a0b9d
Benedicta1 <benedictaojogwu@gmail.com>
2023-06-01 11:56:37 -0700

    lists.h

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -1,6 +1,26 @@
 #ifndef LISTS_H
 #define LISTS_H
 
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * struct list_s - singly linked list
+ * @str: string - (malloc'ed string)
+ * @len: This is the length of the string
+ * @next: This points to the next node
+ *
+ * Description: This is a singly linked list node structure
+ * for my ALX project
+ */
+typedef struct list_s
+{
+	char *str;
+	unsigned int len;
+	struct list_s *next;
+} list_t;
+
 size_t print_list(const list_t *h);
 size_t list_len(const list_t *h);
 list_t *add_node(list_t **head, const char *str);
