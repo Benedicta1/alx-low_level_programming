9a1ca050987cd6ca0e216884c6dd122ceba11e33
Benedicta1 <benedictaojogwu@gmail.com>
2023-09-28 08:35:55 -0700

    Create a function def island_perimeter(grid): that returns the perimeter of the island described in grid:
    grid is a list of list of integers:
    0 represents a water zone
    1 represents a land zone
    One cell is a square with side length 1
    Grid cells are connected horizontally/vertically (not diagonally).
    Grid is rectangular, width and height don’t exceed 100
    Grid is completely surrounded by water, and there is one island (or nothing).
    The island doesn’t have “lakes” (water inside that isn’t connected to the water around the island).

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -10,14 +10,14 @@ def island_perimeter(grid):
 
     perimeter = 0
     for a in range(len(grid)):
-        for b in range(len(grid[0])):
-            if grid[a][b]:
-                if b  == 0 or not grid[a][b - 1]:
+        for b in range(len(grid[a])):
+            if grid[a][b] == 1:
+                if a - 1 < 0 or grid[a - 1][b] == 0:
                     perimeter += 1
-                    if b == len(grid[0]) - 1 or not grid[a][b + 1]:
-                        perimeter += 1
-                        if a == 0 or not grid[a - 1][b]:
-                            perimeter += 1
-                            if a == len(grid) - 1 or not grid[a + 1][b]:
-                                perimeter += 1
-                                return perimeter
+                if b - 1 < 0 or grid[a][b - 1] == 0:
+                    perimeter += 1
+                if b + 1 >= len(grid[a]) or grid[a][b + 1] == 0:
+                    perimeter += 1
+                if a + 1 >= len(grid) or grid[a + 1][b] == 0:
+                    perimeter += 1
+    return perimeter
