d220350712816327b9d9d616759679e67cd0a45a
Benedicta1 <benedictaojogwu@gmail.com>
2023-09-28 04:28:16 -0700

    Create a function def island_perimeter(grid): that returns the perimeter of the island described in grid:
    grid is a list of list of integers:
    0 represents a water zone
    1 represents a land zone
    One cell is a square with side length 1
    Grid cells are connected horizontally/vertically (not diagonally).
    Grid is rectangular, width and height don’t exceed 100
    Grid is completely surrounded by water, and there is one island (or nothing).
    The island doesn’t have “lakes” (water inside that isn’t connected to the water around the island).

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -3,24 +3,28 @@
 
 
 def island_perimeter(grid):
-    """This returns the perimeter of an island
-    Grid is a list of integers in which
-    0 represents a water zone
-    1 represents a land zone"""
+    """Get the result of the  perimeter of an "island" of 1s in an "ocean" of 0s.
 
-    width = len(grid[0])
-    height = len(grid)
-    size = 0
-    margin = 0
+    The  2D grid of 0s and 1s, get the result of the perimeter of the contiguous area of
+    1, with negative space in 0s.
 
-    for a in range(height):
-        for b in range(width):
-            if grid[a][b] == 1:
-                size +=1
-                if (b > 0 and grid[a][b - 1] == 1):
-                    margin +=1
-                    if (a > 0 and grid[a - 1][b] == 1):
-                    margin += 1
-    return size * 4 - margin * 2
+    Args:
+        grid (list of lists of ints): 2D list representation of island
 
+    Attributes:
+        perimeter (int): The total units of cell length around island edge
 
+    """
+    perimeter = 0
+    for a in range(len(grid)):
+        for b in range(len(grid[0])):
+            if grid[a][b]:
+                if b == 0 or not grid[a][b - 1]:
+                    perimeter += 1
+                if b == len(grid[0]) - 1 or not grid[a][b + 1]:
+                    perimeter += 1
+                if a == 0 or not grid[a - 1][b]:
+                    perimeter += 1
+                if a == len(grid) - 1 or not grid[a + 1][b]:
+                    perimeter += 1
+    return perimeter
